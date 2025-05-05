10f752bd525e5bafe1424c98c5101c4409a7defd
Edouard Leroy <leroyedouard@protonmail.com>
2025-05-05 19:39:59 +0200

    work on LowCarbPowerService(generate) fonctionnel - RestController fonctionnel MS fonctionnel

---
+++ src/main/java/io/eddvance/production/lowcarbpower/controller/LowCarbPowerResource.java
@@ -0,0 +1,20 @@
+package io.eddvance.production.lowcarbpower.controller;
+
+import io.eddvance.production.lowcarbpower.service.lowcarbpower_service.LowCarbPowerService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/lowcarbpower")
+public class LowCarbPowerResource {
+    private final LowCarbPowerService lowCarbPowerService;
+    public LowCarbPowerResource(LowCarbPowerService lowCarbPowerService) {
+        this.lowCarbPowerService = lowCarbPowerService;
+    }
+
+    @RequestMapping("/rate")
+    public String getRate(){
+        return lowCarbPowerService.geneRate();
+    }
+}
