13a47e54723351b9e5053f91455ae7cd2cec8125
Edouard Leroy <leroyedouard@protonmail.com>
2025-06-10 19:19:13 +0200

    ServiceCron conf Eureka,H2,Feign ; ajustement entity ; fonctionnel

--- src/main/java/io/eddvance/production/lowcarbpower/LowCarbPowerApplication.java
+++ src/main/java/io/eddvance/production/lowcarbpower/LowCarbPowerApplication.java
@@ -3,9 +3,11 @@ package io.eddvance.production.lowcarbpower;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableDiscoveryClient
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class LowCarbPowerApplication {
 
     public static void main(String[] args) {
